0d5a55d2aa69f90f667154dc0e05049eebb34767
Ivan <ivan.zhivodovsky32@mail.ru>
2021-04-07 16:40:07 +0300

    add new MethodMonteKarloGeometrical

--- Intergral.cs
+++ Intergral.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using MathNet.Symbolics;
 
 namespace NumericalIntegration
@@ -99,5 +100,76 @@ namespace NumericalIntegration
 
             return result;
         }
+
+        double MethodMonteKarloGeometrical(double n)
+        {
+            double yMin = 0;
+            double yMax = 0;
+            Random random = new Random();
+            for (double i = a; i < b; i += 0.001)
+            {
+                double valueFunction = MathParse(funcString, i);
+                
+                if (valueFunction > yMax) yMax = valueFunction;
+                else if (valueFunction < yMin) yMin = valueFunction;
+            }
+
+
+            int k = 0;
+            double xRand, yRand;
+            List<Point> points = new List<Point>();
+            while (points.Count < n)
+            {
+                xRand = random.Next((int)a, (int)b) + random.NextDouble();
+                yRand = random.Next((int)yMin, (int)yMax) + random.NextDouble();
+            }
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                xRand = random.Next((int)a, (int)b) + random.NextDouble();
+                yRand = random.Next((int)yMin, (int)yMax) + random.NextDouble();
+                //yRand = random.Next(-1, 1) + random.NextDouble();
+
+                //else yRand = random.Next((int)yMax, (int)yMin) + random.NextDouble() - 1;
+                double valueFunction = MathParse(funcString, xRand);
+
+                //if (valueFunction > yRand) k++;
+                if ((valueFunction > 0) && (yRand < valueFunction)) k++;
+                else if ((valueFunction < 0) && (yRand > valueFunction)) k++;
+            }
+
+            double area = (b - a) * (yMax - yMin);
+            double result = (k / n) * area;
+
+            return result;
+        }
+
+        //private List<Point> GenerateRandomValues(double xMin, double xMax, double yMin, double yMax, int countPoints)
+        //{
+        //    List<Point> points = new List<Point>();
+        //    Random random = new Random();
+
+        //    double xRand;
+        //    double yRand;
+
+        //    do
+        //    {
+        //        xRand = random.Next((int)xMin, (int)xMax) + random.NextDouble();
+        //        yRand = random.Next((int)yMin, (int)yMax) + random.NextDouble();
+
+        //        if (xRand < 0) if (xRand < xMin) continue;
+        //        if (xRand > 0) if (xRand > xMax) continue;
+
+        //        if (yRand < 0) if (yRand < yMin) continue;
+        //        if (yRand > 0) if (yRand > yMax) continue;
+
+        //        points.Add(new Point(xRand, yRand));
+        //    }
+        //    while (points.Count != countPoints);
+
+        //    return points;
+        //}
     }
 }
